8b93b25a873a10bb76943fb10c85800ad4662b10
pohjus <jussi.pohjolainen@gmail.com>
2023-10-08 12:42:43 +0300

    :sparkles: Add e02/Main.java

--- e02/Main.java
+++ e02/Main.java
@@ -1,3 +1,6 @@
+import java.io.Console;
+import static java.lang.System.out;
+
 /**
  * Main class for exercise.
  *
@@ -9,7 +12,20 @@ public class Main {
      *
      * @param args Command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        Console c = System.console();
 
+        out.println("Anna koko:");
+        char[] arr = new char[Integer.parseInt(c.readLine())];
+        out.println("Anna merkit:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = c.readLine().charAt(0);
+        }
+
+        for (char value : arr) {
+            out.print(value);
+        }
+        out.println();
     }
 }
